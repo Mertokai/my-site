fd08e9202bce2ddc26b3de8c3463720d2e076b56
Mertokai <merteyuboglu.16@hotmail.com>
2023-05-25 23:48:50 +0300

    first commit

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -11,18 +11,18 @@ const Skills = (props: Props): JSX.Element => {
         <div className="mx-auto max-w-7xl space-y-8 py-32 md:space-y-7">
             <h3 className="text-center tracking-[20px]">Skills</h3>
             <Set name="HTML" value="3/5" />
-            <Set name="CSS" value="1/2" />
+            <Set name="CSS" value="3/5" />
             <Set name="JAVASCRIPT" value="3/5" />
             <Set name="REACT" value="3/5" />
-            <Set name="TYPESCRIPT" value="1/2" />
+            <Set name="TYPESCRIPT" value="3/5" />
             <Set name="NEXT13" value="3/5" />
-            <Set name="SANITY" value="1/2" />
-            <Set name="FIREBASE" value="1/2" />
-            <Set name="CLERK" value="1/2" />
+            <Set name="SANITY" value="3/5" />
+            <Set name="FIREBASE" value="3/5" />
+            <Set name="CLERK" value="3/5" />
             <Set name="ASTROJS" value="3/5" />
-            <Set name="TAILWIND" value="1/2" />
+            <Set name="TAILWIND" value="3/5" />
             <Set name="BOOTSTRAP" value="3/5" />
-            <Set name="ENGLISH" value="1/2" />
+            <Set name="ENGLISH" value="3/5" />
         </div>
     )
 }
